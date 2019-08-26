b25631225a12dc73fab44d94d02960dbf48d1815
Christopher Wellons <wellons@nullprogram.com>
2019-08-26 09:43:43 -0400

    speck: Fix aliasing issues using local variables
    This could have also been fixed using the restrict keyword, but this is
    a little more portable.

--- speck/speck.h
+++ speck/speck.h
@@ -15,37 +15,45 @@ speck_init(struct speck *ctx, uint64_t x, uint64_t y)
 {
     ctx->k[0] = y;
     for (uint64_t i = 0; i < 31; i++) {
-        x = (x >> 8) | (x << 56);
+        x = x>>8 | x<<56;
         x += y;
         x ^= i;
-        y = (y << 3) | (y >> 61);
+        y = y<<3 | y>>61;
         y ^= x;
-        ctx->k[i + 1] = y;
+        ctx->k[i+1] = y;
     }
 }
 
 static void
-speck_encrypt(const struct speck *ctx, uint64_t *x, uint64_t *y)
+speck_encrypt(const struct speck *ctx, uint64_t *px, uint64_t *py)
 {
+    uint64_t x = *px;
+    uint64_t y = *py;
     for (int i = 0; i < 32; i++) {
-        *x = (*x >> 8) | (*x << 56);
-        *x += *y;
-        *x ^= ctx->k[i];
-        *y = (*y << 3) | (*y >> 61);
-        *y ^= *x;
+        x = x>>8 | x<<56;
+        x += y;
+        x ^= ctx->k[i];
+        y = y<<3 | y>>61;
+        y ^= x;
     }
+    *px = x;
+    *py = y;
 }
 
 static void
-speck_decrypt(const struct speck *ctx, uint64_t *x, uint64_t *y)
+speck_decrypt(const struct speck *ctx, uint64_t *px, uint64_t *py)
 {
-    for (int i = 0; i < 32; i++) {
-        *y ^= *x;
-        *y = (*y >> 3) | (*y << 61);
-        *x ^= ctx->k[31 - i];
-        *x -= *y;
-        *x = (*x << 8) | (*x >> 56);
+    uint64_t x = *px;
+    uint64_t y = *py;
+    for (int i = 31; i >= 0; i--) {
+        y ^= x;
+        y = y>>3 | y<<61;
+        x ^= ctx->k[i];
+        x -= y;
+        x = x<<8 | x>>56;
     }
+    *px = x;
+    *py = y;
 }
 
 #endif
