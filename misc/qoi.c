fd50f37ca17cedb0bf6709e92fe53fa5c1db497c
Christopher Wellons <wellons@nullprogram.com>
2022-12-06 19:45:45 -0500

    qoi: Switch to pixel-at-a-time decoder
    This gives the caller complete control over the format.

--- misc/qoi.c
+++ misc/qoi.c
@@ -1,103 +1,113 @@
 // QOI decoder in ~100 lines of 32-bit/64-bit freestanding C
 // This is free and unencumbered software released into the public domain.
 
-struct qoimeta {
-    int width, height;
-    unsigned alpha:1, srgb:1;
+struct qoi {
+    unsigned char *p, *end;
+    int width, height, count;
+    unsigned error:1, alpha:1, srgb:1, run:6, c, table[64];
 };
 
-// Validate header and extract image metadata, returning true if the
-// header is valid and reasonable. Multiplying the image's dimensions to
-// compute the image size will not overflow.
-static int qoimeta(struct qoimeta *m, const void *buf, int len)
+// Validate the image header and populate the decoder with the image
+// metadata (width, height, alpha, srgb). Image dimensions can always be
+// multiplied without overflow. If the header is invalid, the error flag
+// will be set immediately.
+//
+// Call the decoder exactly width*height times, or until the error flag
+// is set. Alternatively, keep calling until "count" reaches zero, then
+// the error flag indicates if the entire decode was successful.
+static struct qoi qoiinit(const void *buf, int len)
 {
+    struct qoi qoi = {0, 0, 0, 0, 0, 1, 0, 0, 0, 0xff000000, {0}};
     if (len < 14) {
-        return 0;
+        return qoi;
     }
 
-    const unsigned char *p = buf;
+    unsigned char *p = (void *)buf;
     unsigned g = (unsigned)p[ 0]<<24 | p[ 1]<<16 | p[ 2]<< 8 | p[ 3];
     unsigned w = (unsigned)p[ 4]<<24 | p[ 5]<<16 | p[ 6]<< 8 | p[ 7];
     unsigned h = (unsigned)p[ 8]<<24 | p[ 9]<<16 | p[10]<< 8 | p[11];
     if (g!=0x716f6966U || (!w&&h) || (!h&&w) || p[12]-3u>1 || p[13]>1) {
-        return 0;  // invalid header
+        return qoi;  // invalid header
     }
     if (w>0x7fffffffU || h>0x7fffffffU) {
-        return 0;  // unreasonably huge dimensions
+        return qoi;  // unreasonably huge dimensions
     }
     if ((h && w>0x7fffffffU/h) || (w && h>0x7fffffffU/w)) {
-        return 0;  // multiplying dimensions will overflow
+        return qoi;  // multiplying dimensions will overflow
     }
 
-    m->width  = w;
-    m->height = h;
-    m->alpha  = p[12] == 4;
-    m->srgb   = p[13] == 1;
-    return 1;
+    qoi.p      = p + 14;
+    qoi.end    = p + len;
+    qoi.width  = w;
+    qoi.height = h;
+    qoi.count  = w * h;
+    qoi.error  = 0;
+    qoi.alpha  = p[12]==4;
+    qoi.srgb   = p[13]==1;
+    return qoi;
 }
 
-// Decode image into 32-bit array with length width*height, returning
-// true on success. Trailing junk is permitted and ignored.
-static int qoidecode(unsigned *image, const void *buf, int len)
+// Decode the next ABGR pixel. The error flag is sticky, and it is
+// permitted to continue "decoding" even when the error field is set.
+static unsigned qoinext(struct qoi *q)
 {
-    unsigned char r, g, b, a;
-    const unsigned char *p=buf, *e=p+len;
-    unsigned c=0xff000000U, table[64] = {0};
-    int w = p[ 4]<<24 | p[ 5]<<16 | p[ 6]<<8 | p[ 7];
-    int h = p[ 8]<<24 | p[ 9]<<16 | p[10]<<8 | p[11];
-    unsigned *end = image + w*h;
-
-    for (p += 14; image<end && p<e;) {
-        int v = *p++;
+    if (!q->count || q->error || q->p==q->end) {
+        error: q->error=1, q->count=0;
+        return 0;
+    } else if (q->run) {
+        q->run--;
+    } else {
+        int v = *q->p++;
+        unsigned char *p=q->p, r, g, b, a;
         switch (v&0xc0) {
         case 0x00:  // INDEX (spec: "must not" repeat; but real images do)
-            c = *image++ = table[v&63];
-            continue;
+            q->c = q->table[v&63];
+            break;
         case 0x40:  // DIFF
-            r=c, g=c>>8, b=c>>16, a=c>>24;
+            r=q->c, g=q->c>>8, b=q->c>>16, a=q->c>>24;
             r += (v>>4 & 3) - 2;
             g += (v>>2 & 3) - 2;
             b += (v>>0 & 3) - 2;
-            c = *image++ = r | g<<8 | b<<16 | (unsigned)a<<24;
+            q->c = r | g<<8 | b<<16 | (unsigned)a<<24;
             break;
         case 0x80:  // LUMA
             v = (v&63) - 32;
-            if (e-p < 1) {
-                return 0;
-            }
-            r=c, g=c>>8, b=c>>16, a=c>>24;
-            r += (*p >> 4) - 8 + v;
-            g +=                 v;
-            b += (*p++&15) - 8 + v;
-            c = *image++ = r | g<<8 | b<<16 | (unsigned)a<<24;
+            if (q->end-p < 1) goto error;
+            r=q->c, g=q->c>>8, b=q->c>>16, a=q->c>>24;
+            r += v + (*p>>4) - 8;
+            g += v;
+            b += v + (*p&15) - 8;
+            q->c = r | g<<8 | b<<16 | (unsigned)a<<24;
+            q->p += 1;
             break;
         case 0xc0:
             switch(v&=63) {
             case 63:  // RGB
-                if (e-p < 4) return 0;
-                c = *image++ = p[0] | p[1]<<8 | p[2]<<16 | (unsigned)p[3]<<24;
-                p += 4;
+                if (q->end-p < 4) goto error;
+                q->c = p[0] | p[1]<<8 | p[2]<<16 | (unsigned)p[3]<<24;
+                q->p += 4;
                 break;
             case 62:  // RGB
-                if (e-p < 3) return 0;
-                r=p[0], g=p[1], b=p[2], a=c>>24;
-                p += 3;
-                c = *image++ = r | g<<8 | b<<16 | (unsigned)a<<24;
+                if (q->end-p < 3) goto error;
+                r=p[0], g=p[1], b=p[2], a=q->c>>24;
+                q->c = r | g<<8 | b<<16 | (unsigned)a<<24;
+                q->p += 3;
                 break;
             default:  // RUN
-                if (end-image < v) return 0;
-                do {
-                    *image++ = c;
-                } while (v--);
-                continue;
+                if (q->count < v) goto error;
+                q->run = v;
+                break;
             }
         }
-        r=c, g=c>>8, b=c>>16, a=c>>24;
-        table[(r*3 + g*5 + b*7 + a*11)&63] = c;
+        r=q->c, g=q->c>>8, b=q->c>>16, a=q->c>>24;
+        q->table[(r*3 + g*5 + b*7 + a*11)&63] = q->c;
     }
 
-    return image==end && e-p>=8 &&
-        !p[0] && !p[1] && !p[2] && !p[3] && !p[4] && !p[5] && !p[6] && p[7]==1;
+    if (!--q->count) {
+        q->error |= q->end-q->p<8 || q->p[0] || q->p[1] || q->p[2] ||
+            q->p[3] || q->p[4] || q->p[5] || q->p[6] || q->p[7]!=1;
+    }
+    return q->c;
 }
 
 
@@ -107,9 +117,6 @@ static int qoidecode(unsigned *image, const void *buf, int len)
 //   $ ./qoi2ff <example.qoi >example.ff
 #include <stdio.h>
 
-#define MAX_QOILEN (1<<28)  // 256MiB
-#define MAX_PIXELS 100000000
-
 int main(void)
 {
     #if _WIN32
@@ -118,17 +125,9 @@ int main(void)
     _setmode(1, 0x8000);
     #endif
 
-    static char buf[MAX_QOILEN];
+    static char buf[1<<28];
     int len = fread(buf, 1, sizeof(buf), stdin);
-
-    struct qoimeta qoi;
-    static unsigned image[MAX_PIXELS];
-    if (!qoimeta(&qoi, buf, len) || qoi.width*qoi.height > MAX_PIXELS) {
-        return 1;
-    }
-    if (!qoidecode(image, buf, len)) {
-        return 1;
-    }
+    struct qoi qoi = qoiinit(buf, len);
 
     char header[] = {
         'f', 'a', 'r', 'b', 'f', 'e', 'l', 'd',
@@ -137,15 +136,15 @@ int main(void)
     };
     fwrite(header, sizeof(header), 1, stdout);
 
-    for (int i = 0; i < qoi.width*qoi.height; i++) {
-        unsigned c = image[i];
-        unsigned char r=c, g=c>>8, b=c>>16, a=c>>24;
+    while (qoi.count) {
+        unsigned c = qoinext(&qoi);
+        unsigned char r=c, g=c>>8, b=c>>16, a=c>>24; // TODO: sRGB handling
         unsigned char pixel[] = {
             r, -(r&1), g, -(g&1), b, -(b&1), a, -(a&1)
         };
         fwrite(pixel, sizeof(pixel), 1, stdout);
     }
     fflush(stdout);
-    return ferror(stdout) || ferror(stdin);
+    return qoi.error || ferror(stdout) || ferror(stdin);
 }
 #endif
