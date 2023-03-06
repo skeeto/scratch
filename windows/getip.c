1337952dc84c060628472695a6b564df0dc9cf23
Christopher Wellons <wellons@nullprogram.com>
2023-03-06 10:41:12 -0500

    getip.c: Modernize and improve robustness
    I've learned a lot since I wrote this a year ago. First, output buffers
    are easy, so use one. Second, x86 should stack-align on entry. Third,
    use ExitProcess to work around race conditions in Windows, in this case
    wininet.dll.
    
    As for "modernization" my style has changed. First, no more return types
    on their own line. Instead use typedef for shorter type names. Second,
    no more const since it is not worth its weight. Third, update the build
    instructions to my current understanding. No more GCC "freestanding" and
    MSVC "/Os" does not work as advertised, so stop using it. Fourth, stop
    dereferencing unqualified "char *" pointers.

--- windows/getip.c
+++ windows/getip.c
@@ -1,10 +1,10 @@
 // getip - determine and print public IP address to standard output
 //
 // Mingw-w64:
-//    $ cc -Os -ffreestanding -fno-ident -fno-asynchronous-unwind-tables \
-//         -s -nostdlib -o getip.exe getip.c -lkernel32 -lwininet
+//    $ cc -Os -fno-asynchronous-unwind-tables -s -nostartfiles
+//         -Wl,--gc-sections -o getip.exe getip.c -lwininet
 // MSVC:
-//    C:\>cl /Os /GS- getip.c
+//    C:\>cl /O2 /GS- getip.c
 //
 // Note: Unfortunately this program usually hangs on exit due to an old DLL
 // unload deadlock defect in wininet.dll. Please fix this, Microsoft!
@@ -24,8 +24,7 @@
 // Decode numeric, ASCII HTML entities in place. Returns the decoded length.
 // Invalid entities, non-ASCII entities, and control bytes are all decoded
 // to space. Stops at the first comma.
-static int
-decode(char *buf, int len)
+static int decode(unsigned char *buf, int len)
 {
     int n = 0, s = 0, e = 0;
     for (int i = 0; i < len; i++) {
@@ -43,10 +42,10 @@ decode(char *buf, int len)
                 case '0': case '1': case '2': case '3': case '4':
                 case '5': case '6': case '7': case '8': case '9':
                           e = e*10 + buf[i] - '0';
-                          e = e >= 127 ? 127 : e;  // no overflow
+                          e = e>=127 ? 127 : e;  // no overflow
                           break;
                 case ';': if (e == ',') return n;
-                          buf[n++] = e < 32 || e >= 127 ? ' ' : e;
+                          buf[n++] = e<32 || e>=127 ? ' ' : (unsigned char)e;
                           e = s = 0;
                           break;
                 default : e = 127;
@@ -56,47 +55,71 @@ decode(char *buf, int len)
     return n;
 }
 
+#define SUCCESS (Err){0, 0}
+#define ERR(s) (Err){(unsigned char *)s, sizeof(s)-1}
+typedef struct {
+    unsigned char *err;
+    int len;
+} Err;
+
 // Returns an error message, or null on success.
-static const char *
-run(void)
+static Err run(void)
 {
     HINTERNET h = InternetOpenA("wininet", INTERNET_OPEN_TYPE_DIRECT, 0, 0, 0);
-    if (!h) return "InternetOpenA()";
+    if (!h) return ERR("InternetOpenA()");
 
     HINTERNET r = InternetOpenUrlA(h, URL, 0, 0, 0, 0);
-    if (!r) return "InternetOpenUrlA()";
+    if (!r) return ERR("InternetOpenUrlA()");
 
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD n;
-    char buf[256];
+    unsigned char buf[256];
     if (!InternetReadFile(r, buf, sizeof(buf)-1, &n)) {
-        return "InternetReadFile()";
+        return ERR("InternetReadFile()");
     }
 
     n = decode(buf, n);
     buf[n++] = '\n';
     if (!WriteFile(out, buf, n, &n, 0)) {
-        return "WriteFile()";
+        return ERR("WriteFile()");
     }
 
     InternetCloseHandle(r);
     InternetCloseHandle(h);
-    return 0;
+    return SUCCESS;
 }
 
-int
-mainCRTStartup(void)
+typedef struct {
+    int off;
+    unsigned char buf[128];
+} Buf;
+
+static void append(Buf *b, void *src, int len)
 {
-    const char *err = run();
-    if (err) {
-        HANDLE h = GetStdHandle(STD_ERROR_HANDLE);
-        static const char pre[] = "getip: ";
-        static const char suf[] = " failure\n";
-        DWORD n;
-        WriteFile(h, pre, sizeof(pre)-1, &n, 0);
-        WriteFile(h, err, lstrlenA(err), &n, 0);
-        WriteFile(h, suf, sizeof(suf)-1, &n, 0);
-        return 1;
+    unsigned char *s = src;
+    int avail = sizeof(b->buf) - b->off;
+    int count = len<avail ? len : avail;
+    for (int i = 0; i < count; i++) {
+        b->buf[b->off++] = s[i];
     }
-    return 0;
+}
+
+#if __i686__
+__attribute__((force_align_arg_pointer))
+#endif
+void mainCRTStartup(void)
+{
+    int ret = 0;
+    Err err = run();
+    if (err.err) {
+        HANDLE h = GetStdHandle(STD_ERROR_HANDLE);
+        Buf buf = {0};
+        append(&buf, "getip: ", 7);
+        append(&buf, err.err, err.len);
+        append(&buf, " failure\n", 9);
+        DWORD n;
+        WriteFile(h, buf.buf, buf.off, &n, 0);
+        ret = 1;
+    }
+    ExitProcess(ret);
 }
