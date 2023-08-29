e77376dd48397252c858c0e2c42b0093bc4b4c35
Christopher Wellons <wellons@nullprogram.com>
2023-08-29 18:43:16 -0400

    slice.c: Use "len >= cap" so it asserts in grow_()
    Costs nothing but helps catch mistakes.

--- misc/slice.c
+++ misc/slice.c
@@ -101,7 +101,7 @@ static byte *alloc(arena *a, size objsize, size align, size count)
 // only evaluated once and may safely have side effects.
 #define push(arena, sliceptr) ({ \
     typeof(sliceptr) s_ = (sliceptr); \
-    if (s_->len == s_->cap) { \
+    if (s_->len >= s_->cap) { \
         grow_((arena), (byte *)s_, sizeof(*s_->data), alignof(*s_->data)); \
     } \
     s_->data + s_->len++; \
