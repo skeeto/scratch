af03b7cb6ca4a623a102b972f34e22ed8926e48e
Christopher Wellons <wellons@nullprogram.com>
2023-08-31 23:31:00 -0400

    slice.c: More tidying up
    The byte type is an aliasing type referring to raw memory, mainly for
    allocation and such, while u8 is for non-aliasing octets like variables
    containing UTF-8 text.
    
    I considered __builtin_memcpy(), but then I remembered it has the same
    crummy semantics as the standard version, so I wrote copy() instead.

--- misc/slice.c
+++ misc/slice.c
@@ -1,13 +1,14 @@
 // Arena-backed, generic slices (experiment)
-
+//
 // The main feature is the type-generic push() macro with supporting
 // grow_() function. It allocates out of a simple arena with non-local
 // goto for out-of-memory errors. On resize, the old buffer is left in
 // place and continues to be a valid copy so long as its arena keeps it
-// alive. New elements are zero-initialized. The push macro resolves to
-// a pointer to the new element. Callers first define a (data, len, cap)
-// struct, and grow_ operates on it by memcpy-based type punning.
-
+// alive. Elements are zero-initialized when newly-allocated. The push
+// macro evalutes to a pointer to the newly-pushed element. Callers
+// define a (data, len, cap) struct, and grow_ operates on it by
+// memcpy-based type punning.
+//
 //   struct struct {
 //       item *data;
 //       size  len;
@@ -39,6 +40,7 @@
 
 typedef unsigned char      byte;
 typedef __PTRDIFF_TYPE__   size;
+typedef __UINT8_TYPE__     u8;
 typedef unsigned short     u16;
 typedef   signed int       b32;
 typedef   signed int       i32;
@@ -51,14 +53,22 @@ typedef unsigned long long u64;
 #define lengthof(s) (countof(s) - 1)
 #define assert(c)   while (!(c)) __builtin_unreachable()
 
-static b32 fullwrite(i32, byte *, size);
+static b32 fullwrite(i32, u8 *, size);
 
-// Arena
+// Memory
 
 #define new(...)              newx(__VA_ARGS__, new3, new2)(__VA_ARGS__)
 #define newx(a, b, c, d, ...) d
 #define new2(a, t)            (t *)alloc(a, sizeof(t), alignof(t), 1)
 #define new3(a, t, n)         (t *)alloc(a, sizeof(t), alignof(t), n)
+
+static byte *copy(byte *dst, byte *src, size len)
+{
+    for (size i = 0; i < len; i++) {
+        dst[i] = src[i];
+    }
+    return dst + len;
+}
 
 typedef struct {
     byte *mem;
@@ -116,35 +126,27 @@ static void grow_(arena *a, byte *header, size objsize, size align)
         byte *data;
         size  len;
         size  cap;
-    } copy;
-    byte *copyp = (byte *)&copy;
+    } replica;
+    copy((byte *)&replica, header, sizeof(replica));
+    assert(replica.cap >= 0);
+    assert(replica.len >= 0);
+    assert(replica.len <= replica.cap);
 
-    for (size i = 0; i < sizeof(copy); i++) {
-        copyp[i] = header[i];
+    if (!replica.cap) {
+        replica.cap = 1;
     }
-    assert(copy.cap >= 0);
-    assert(copy.len >= 0);
-    assert(copy.len <= copy.cap);
+    byte *data = alloc(a, objsize*2, align, replica.cap);
+    copy(data, replica.data, objsize*replica.len);
 
-    if (!copy.cap) {
-        copy.cap = 1;
-    }
-    byte *data = alloc(a, objsize*2, align, copy.cap);
-    for (size i = 0; i < objsize*copy.len; i++) {
-        data[i] = copy.data[i];
-    }
-
-    copy.data = data;
-    copy.cap *= 2;
-    for (size i = 0; i < sizeof(copy); i++) {
-        header[i] = copyp[i];
-    }
+    replica.data = data;
+    replica.cap *= 2;
+    copy(header, (byte *)&replica, sizeof(replica));
 }
 
 // Test program
 
 typedef struct {
-    byte *buf;
+    u8   *buf;
     size  cap;
     size  len;
     b32   err;
@@ -158,16 +160,13 @@ static void flush(bout *o)
     }
 }
 
-static void write(bout *o, byte *buf, size len)
+static void write(bout *o, u8 *buf, size len)
 {
-    byte *end = buf + len;
+    u8 *end = buf + len;
     while (!o->err && buf<end) {
         size avail = o->cap - o->len;
         size count = avail<end-buf ? avail : end-buf;
-        byte *dst = o->buf + o->len;
-        for (size i = 0; i < count; i++) {
-            dst[i] = buf[i];
-        }
+        copy(o->buf+o->len, buf, count);
         buf += count;
         o->len += count;
         if (o->len == o->cap) {
@@ -178,18 +177,18 @@ static void write(bout *o, byte *buf, size len)
 
 static void print(bout *o, i32 x, size width)
 {
-    byte buf[32];
-    byte *end = buf + countof(buf);
-    byte *beg = end;
+    u8 buf[32];
+    u8 *end = buf + countof(buf);
+    u8 *beg = end;
     i32 t = x<0 ? x : -x;
     do {
-        *--beg = '0' - (byte)(t % 10);
+        *--beg = '0' - (u8)(t % 10);
     } while (t /= 10);
     if (x < 0) {
         *--beg = '-';
     }
     for (size i = 0; i < width - (end - beg); i++) {
-        write(o, (byte *)" ", 1);
+        write(o, (u8 *)" ", 1);
     }
     write(o, beg, end-beg);
 }
@@ -219,14 +218,14 @@ static u32 run(byte *heap, size heaplen)
 {
     arena *a = newarena(heap, heaplen);
     if (__builtin_setjmp(a->oom)) {
-        static byte msg[] = "out of memory\n";
+        static u8 msg[] = "out of memory\n";
         fullwrite(2, msg, lengthof(msg));
         return 2;
     }
 
     bout *stdout = new(a, bout);
     stdout->cap = 1<<12;
-    stdout->buf = new(a, byte, stdout->cap);
+    stdout->buf = new(a, u8, stdout->cap);
 
     i32 nbins = 100;
     i32slice *bins = new(a, i32slice, nbins);
@@ -239,12 +238,12 @@ static u32 run(byte *heap, size heaplen)
 
     for (i32 b = 0; b < nbins; b++) {
         print(stdout, b, 2);
-        write(stdout, (byte *)": ", 2);
+        write(stdout, (u8 *)": ", 2);
         for (size i = 0; i < bins[b].len; i++) {
             print(stdout, bins[b].data[i], 3);
-            write(stdout, (byte *)" ", 1);
+            write(stdout, (u8 *)" ", 1);
         }
-        write(stdout, (byte *)"\n", 1);
+        write(stdout, (u8 *)"\n", 1);
     }
 
     flush(stdout);
@@ -261,9 +260,9 @@ typedef struct {} *handle;
 #define W32 __attribute((dllimport, stdcall))
 W32 void   ExitProcess(u32) __attribute((noreturn));
 W32 handle GetStdHandle(u32);
-W32 b32    WriteFile(handle, byte *, u32, u32 *, void *);
+W32 b32    WriteFile(handle, u8 *, u32, u32 *, void *);
 
-static b32 fullwrite(i32 fd, byte *buf, size len)
+static b32 fullwrite(i32 fd, u8 *buf, size len)
 {
     assert((size)(u32)len == len);
     handle out = GetStdHandle(-10 - fd);
@@ -286,7 +285,7 @@ void mainCRTStartup(void)
 // $ cc -nostdlib -fno-builtin -o slice slice.c
 // $ ./slice
 
-static b32 fullwrite(i32 fd, byte *buf, size len)
+static b32 fullwrite(i32 fd, u8 *buf, size len)
 {
     for (size off = 0; off < len;) {
         size r;
