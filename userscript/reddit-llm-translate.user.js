2352d5a655ee5a88f997a0eee4ac50f8b1861a88
Christopher Wellons <wellons@nullprogram.com>
2024-10-23 23:31:53 -0400

    Add reddit LLM translation script
    That's right: I recently dove head-first into the world of LLMs. It's
    simultaneously amazing and disappointing. This world is in constant
    motion, and my LLM recommendation in the instructions will look quaint
    and dated only a year from now.

---
+++ userscript/reddit-llm-translate.user.js
@@ -0,0 +1,77 @@
+// ==UserScript==
+// @name         Reddit translation via "on-prem" Large Language Model (LLM)
+// @description  Usertext elements are passed to a local LLM for translation
+// @lastupdated  2024-10-22
+// @version      1.0
+// @license      Public Domain
+// @include      https://old.reddit.com/*
+// @grant        none
+// ==/UserScript==
+
+// Browser instructions:
+// 1. Set up the LLM as instructed below
+// 2. Install this user script (Greasemonkey, etc.)
+// 3. Press ALT-t on reddit to begin translating posts and comments
+//
+// Longer comments take more time. Typical comments take a couple of
+// seconds depending on your local hardware. The LLM auto-detects the
+// source language and can handle pages with a mixture of languages.
+//
+// LLM and server instructions (any operating system):
+// 1. Install a C++ compiler (w64devkit, etc.)
+// 2. $ git clone https://github.com/ggerganov/llama.cpp
+// 3. $ make -j$(nproc)
+// 4. Open https://huggingface.co/bartowski/gemma-2-2b-it-GGUF
+// 5. Download gemma-2-2b-it-Q4_K_M.gguf
+// 6. $ ./llama-server -m gemma-2-2b-it-Q4_K_M.gguf -c 8192
+//
+// To reply in the foreign language, visit the local web interface to
+// your LLM, http://localhost:8080/, and kindly ask it to translate your
+// comment into the target language. If you don't know what the local
+// language is called, give the LLM a sample and request identification.
+//
+// Advanced option: gemma-2-9b-it produces slightly better translations
+// but is slower and consumes more resources. Even more important when
+// translating your own outgoing comments. Mistral-Nemo-Instruct-2407 is
+// also a good choice. In either case, probably only worth using if you
+// can run inference at least partially on a GPU, where they're faster
+// than gemma-2-2b-it on a CPU.
+//
+// With some work, this script may support other OpenAI-compatable APIs.
+// Most are stricter than llama.cpp, and likely require an API token.
+
+async function translate(tag, lang = "English") {
+  let url = "http://localhost:8080/v1/chat/completions"
+  let response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      messages: [{
+        role: "system",
+        content:
+          `You are a translator that translates user messages into ${lang}. ` +
+          `Do not respond. Do not explain. Do not elaborate. Only translate. ` +
+          `Preserve HTML tags.`
+      }, {
+        role: "user",
+        content: tag.innerHTML,
+      }],
+    }),
+  })
+  if (response.ok) {
+      let message = await response.json()
+      tag.innerHTML = message.choices[0].message.content
+  }
+}
+
+async function translateAll() {
+  let elements = document.querySelectorAll('.usertext-body .md')
+  for (let i = 1; i < elements.length; i++) {
+    await translate(elements[i])
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.altKey && e.key === "t") {
+    translateAll()
+  }
+})
