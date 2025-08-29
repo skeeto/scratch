e06aeb1a36a31260cc2763ac4ed84ec18548ae47
Christopher Wellons <wellons@nullprogram.com>
2025-08-29 09:28:27 -0400

    Add xcancel.user.js

---
+++ userscript/xcancel.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Redirect x.com to xcancel.com
+// @description  Switches to an actually-functioning website
+// @lastupdated  2024-12-23
+// @version      1.0
+// @license      Public Domain
+// @include      https://x.com/*
+// @grant        none
+// ==/UserScript==
+
+location.hostname = "xcancel.com"
